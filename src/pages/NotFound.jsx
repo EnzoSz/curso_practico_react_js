f65b6ddceda25258a100529a5c0cbbafff89859a
Enzo Soliz <enzosoliz95@gmail.com>
2023-02-22 18:16:17 -0300

    Navengando entre rutas

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div>
+            No encotrado, Error
+        </div>
+    );
+}
+export default NotFound;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout.jsx';
 import Login from '../containers/Login.jsx';
 import RecoveryPassword from "../containers/RecoveryPassword.jsx";
+import Home from "../pages/Home.jsx";
+import NotFound from "../pages/NotFound.jsx";
 import '../styles/global.css';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Layout>
-                    <Route exact path="/" component= {Home}/>
-                    <Route exact path="/login" component= {Login} />
-                    <Route exact path="recovery-password" component= {RecoveryPassword} />
-                    <Route component={NotFound}/>
-                </Layout>
-            </Switch>
+            <Layout>
+                <Routes>
+                    <Route  path="/" element= {<Home/>}/>
+                    <Route  path="/login" element= {<Login/>} />
+                    <Route  path="/recovery-password" element= {<RecoveryPassword/>} />
+                    <Route  path= "*" element={<NotFound/>}/>
+                </Routes>
+            </Layout>
+            
         </BrowserRouter>
         
     );
