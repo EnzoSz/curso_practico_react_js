d4e5ffc350eca5d99ff329d84911f6a490003d74
Enzo Soliz <enzosoliz95@gmail.com>
2023-03-16 10:40:26 -0300

    useRef y formularios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import '../styles/Login.scss';
 
 const Login = ()=> {
+    const form = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(form.current);
+        const data = {
+            usenanme : formData.get('email'),
+            password: formData.get('password')
+        }
+        console.log(data);
+    }    
     return (
         <div className="Login">
             <div className="Login-container">
@@ -10,15 +21,17 @@ const Login = ()=> {
                 <h1 className="title">Create a new password</h1>
                 <p className="subtitle">Enter a new passwrd for yue account</p>
     
-                <form action="/" className="form">
+                <form action="/" className="form" ref={form}>
+                    <label htmlFor="email"className="label">Email addres</label>
+                    <input type="text" name="email"placeholder="platzi@example.com" className="input input-email"/>
                     <label for="password" className="label">Password</label>
-                    <input type="password" id="password" placeholder="*********" className="input input-password" />
-        
-                    <label for="new-password" className="label">Password</label>
-                    <input type="password" id="new-password" placeholder="*********" className="input input-password" />
-        
-                    <input type="submit" value="Confirm" className="primary-button login-button"/>
+                    <input type="password" name="password" placeholder="*********" className="input input-password" />
+                    <button type="submit" onClick={handleSubmit} className="primary-button login-button">
+                    Log in
+                    </button>
+                    <a href="/">Forgot my password</a>
                 </form>
+                <button className="secondary-button signup-button" >Sign up</button>
             </div>
         </div>
     );
