45dd54ceb8ad0868617b7859c4b1fbbf09dacc4d
Enzo Soliz <enzosoliz95@gmail.com>
2023-03-16 11:27:18 -0300

    React context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,35 +4,41 @@ import Layout from '../containers/Layout.jsx';
 import Home from "../pages/Home.jsx";
 import Login from '../pages/Login.jsx';
 import PasswordRecovery from "../pages/PasswordRecovery.jsx";
-import SendEmail from  '../pages/SendEmail.jsx';
+import SendEmail from '../pages/SendEmail.jsx';
 import NewPassword from '../pages/NewPassword.jsx';
 import MyAccount from '../pages/MyAccount.jsx';
 import CreateAccount from '../pages/CreateAccount.jsx';
 import Checkout from '../pages/Checkout.jsx';
 import Orders from '../pages/Orders.jsx';
 import NotFound from "../pages/NotFound.jsx";
+import AppContext from "../context/AppContext.js";
+import useInitialState from "../hooks/useInitialState.js";
 import '../styles/global.css';
 
+
 const App = () => {
+    const initialState = useInitialState();
     return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route  path="/" element= {<Home/>}/>
-                    <Route  path="/login" element= {<Login/>} />
-                    <Route  path="/recovery-password" element= {<PasswordRecovery/>} />
-                    <Route  path="/send-email" element={<SendEmail/>} />
-					<Route  path="/new-password" element={<NewPassword/>} />
-					<Route  path="/account" element={<MyAccount/>} />
-					<Route  path="/signup" element={<CreateAccount/>} />
-					<Route  path="/checkout" element={<Checkout/>} />
-					<Route  path="/orders" element={<Orders/>} />
-                    <Route  path= "*" element={<NotFound/>}/>
-                </Routes>
-            </Layout>
-            
-        </BrowserRouter>
-        
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/recovery-password" element={<PasswordRecovery />} />
+                        <Route path="/send-email" element={<SendEmail />} />
+                        <Route path="/new-password" element={<NewPassword />} />
+                        <Route path="/account" element={<MyAccount />} />
+                        <Route path="/signup" element={<CreateAccount />} />
+                        <Route path="/checkout" element={<Checkout />} />
+                        <Route path="/orders" element={<Orders />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
+
+
     );
 }
 
